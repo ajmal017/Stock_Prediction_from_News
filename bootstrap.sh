bb57834e84ee98e42359c8181ff0ef818a98bbdb
gargankush <anks.garg@gmail.com>
2020-11-02 19:02:40 +0000

    updated project and HW4

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo python3 -m pip install \
+    botocore \
+    boto3 \
+    ujson \
+    beautifulsoup4  \
+    lxml \
+	yfinance \
+	yahooquery \
+	DateTime \
+	python-csv \
+	os-sys \
+	logging \
+	scipy \
+	scikit-learn 
